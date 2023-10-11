605bc1abb5ffd17773da4242a96addf6c54cb9cd
yudhosaktirama <yudhosakti89@gmail.com>
2023-10-11 18:54:17 +0700

    Membuat sebuah interface bernama WordDao.kt untuk menyimpan query yang nantinya akan menjadi pengubung antara activity/fragment dengan database sqlite

---
+++ app/src/main/java/com/example/lesson_9_database/WordDao.kt
@@ -0,0 +1,17 @@
+package com.example.lesson_9_database
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+interface WordDao {
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): List<Word>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
