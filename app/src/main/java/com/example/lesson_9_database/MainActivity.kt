cac5e1aeb4b2c1a44357e5f0bb49f6e2e7ae6e06
ritaapr <aprilritaapr@gmail.com>
2023-10-13 12:12:39 +0700

    Menambahkan RecyclerView dalam metode onCreate() dari MainActivity.

--- app/src/main/java/com/example/lesson_9_database/MainActivity.kt
+++ app/src/main/java/com/example/lesson_9_database/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.lesson_9_database
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
